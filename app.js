a0fa4e80db5ded24ef85700207e58c6fc9824325
Leticia <leticia.chamarelli@outlook.com>
2023-03-03 17:03:09 -0300

    importações realizadas

---
+++ app.js
@@ -0,0 +1,9 @@
+// imports - importações 
+require('dotenv').config()
+const express = require('express')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const app = express()
+
+
