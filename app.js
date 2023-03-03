937b19dd810c76f3fb42be4290e9f81931532dbb
Leticia <leticia.chamarelli@outlook.com>
2023-03-03 18:05:03 -0300

    criando e validando usuários

--- app.js
+++ app.js
@@ -6,9 +6,38 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
+//Config JSON response - configurando resposta em JSON
+app.use(express.json())
+
+//Models
+const User = require('./models/User')
+
 //Open/Public Route - rota pública/aberta
 app.get('/', (req, res) => {
-    res.status(200).json({mgs: "Bem vindo a nossa API"})
+    res.status(200).json({mgs: 'Bem vindo a nossa API'})
+})
+
+//Register User - Registrando usuário
+app.post('/auth/register', async(req, res) => {
+
+    const {name, email, password, confirmPassword} = req.body
+
+    //validations - validações 
+    if(!name) {
+        return res.status(422).json({msg: 'O nome é obrigatório'})
+    }
+
+    if(!email) {
+        return res.status(422).json({msg: 'O e-mail é obrigatório'})
+    }
+
+    if(!password) {
+        return res.status(422).json({msg: 'A senha é obrigatória'})
+    }
+
+    if(password !== confirmPassword) {
+        return res.status(422).json({msg: 'As senhas não conferem'})
+    }
 })
 
 app.listen(8000)
